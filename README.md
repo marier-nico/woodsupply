61033bc6f8f0a045018ca12d72e495fac6dd01d1
Nicolas Marier <software@nmarier.com>
2020-06-11 22:01:22 -0400

    Use env variables during build

--- README.md
+++ README.md
@@ -8,8 +8,18 @@ The scoreboard provides nice visualizations for game scores, that's really all t
 
 # Librairies
 
-    - Layout : [react-grid-layout](https://github.com/STRML/react-grid-layout)
-    - Charts : [nivo](https://nivo.rocks/)
-    - Tables : [react-bootstrap](https://react-bootstrap.github.io/)
-    - Burger menu : [react-burger-menu](https://github.com/negomi/react-burger-menu)
-    - Icons : [react-icons](https://react-icons.github.io/react-icons/)
+- Layout : [react-grid-layout](https://github.com/STRML/react-grid-layout)
+- Charts : [nivo](https://nivo.rocks/)
+- Tables : [react-bootstrap](https://react-bootstrap.github.io/)
+- Burger menu : [react-burger-menu](https://github.com/negomi/react-burger-menu)
+- Icons : [react-icons](https://react-icons.github.io/react-icons/)
+
+# Env Variables
+
+Environment variables used in this project are listed below, and should be set in `.env`.
+These defaults shouldn't really change in this repository, but they are very convenient for
+automated builds.
+
+- `REACT_APP_MAIN_SERVER_URL`=`"http://localhost:8000/"`
+- `REACT_APP_API_ROUTE_SUFFIX`=`""`
+- `PUBLIC_URL`=`"http://localhost:3000/"`
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,9 @@
-const server_url = "http://localhost:8000/";
+const server_url = process.env.REACT_APP_MAIN_SERVER_URL;
+const route_suffix = process.env.REACT_APP_API_ROUTE_SUFFIX || "";
 
 async function queryScores(teamNames, serverUrl) {
   let body = JSON.stringify({ team_names: teamNames });
-  let response = await fetch(`${serverUrl}scores`, {
+  let response = await fetch(`${serverUrl}scores${route_suffix}`, {
     method: "POST",
     body: body,
   });
@@ -12,7 +13,7 @@ async function queryScores(teamNames, serverUrl) {
 
 async function queryCompetition(competitionName) {
   let body = JSON.stringify({ competition_name: competitionName });
-  let response = await fetch(`${server_url}competition_teams`, {
+  let response = await fetch(`${server_url}competition_teams${route_suffix}`, {
     body: body,
     method: "POST",
   });
@@ -21,19 +22,19 @@ async function queryCompetition(competitionName) {
 }
 
 async function queryCompetitions() {
-  let response = await fetch(`${server_url}competitions`);
+  let response = await fetch(`${server_url}competitions${route_suffix}`);
   let data = await response.json();
   return data.competitions;
 }
 
 async function queryTeams(server_url) {
-  let response = await fetch(`${server_url}teams`);
+  let response = await fetch(`${server_url}teams${route_suffix}`);
   let data = await response.json();
   return data.team_names;
 }
 
 async function queryServers() {
-  let response = await fetch(`${server_url}servers`)
+  let response = await fetch(`${server_url}servers${route_suffix}`)
   let data = await response.json()
   return data.servers;
 }
